f107afb0d148c2a5fe5f8fcdddcc01b3a9e4a5c9
spaceribs <spaceribs@gmail.com>
2014-04-02 13:56:06 -0400

    Removes the buggy fastPartition that was hacked in yesterday

--- src/ch/k42/metropolis/plugin/PluginConfig.java
+++ src/ch/k42/metropolis/plugin/PluginConfig.java
@@ -26,9 +26,6 @@ public class PluginConfig {
     private static final String pathDirectionFallbackPlacing = "generator.enableDirectionFallbackPlacing";
     private static boolean directionFallbackPlacing = false;
 
-    private static final String pathFastPlacement = "generator.enableFastPlacement";
-    private static boolean fastPlacement = false;
-
     private static final String pathBuildChance = "generator.buildChance";
     private static int buildChance = 80;
 
@@ -54,7 +51,7 @@ public class PluginConfig {
     private static int caveChance = 20;
 
     private static final String pathNoLaggRelighting = "generator.noLaggRelighting";
-    private static boolean noLaggRelighting = false;
+    private static boolean  noLaggRelighting = false;
 
     private PluginConfig() {}
 
@@ -80,10 +77,6 @@ public class PluginConfig {
 
     public static boolean allowDirectionFallbackPlacing() {
         return directionFallbackPlacing;
-    }
-
-    public static boolean fastPlacementEnabled() {
-        return fastPlacement;
     }
 
     public static boolean getNoLaggRelighting() {
@@ -129,7 +122,6 @@ public class PluginConfig {
         buildHeight = configFile.getInt(pathBuildHeight,buildHeight);
         caveChance = configFile.getInt(pathCaveChance,caveChance);
         noLaggRelighting = configFile.getBoolean(pathNoLaggRelighting, noLaggRelighting);
-        fastPlacement = configFile.getBoolean(pathFastPlacement, fastPlacement);
     }
 
 }

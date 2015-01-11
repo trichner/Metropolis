ceea56211128c4eea2bbdb12a21cd589b9436c18
Thomas Richner <thomasrichner+github@gmail.com>
2015-01-11 04:58:31 +0100

    cleanup

--- src/main/java/ch/k42/metropolis/generator/cuboid/CuboidRotator.java
+++ src/main/java/ch/k42/metropolis/generator/cuboid/CuboidRotator.java
@@ -2,12 +2,15 @@ package ch.k42.metropolis.generator.cuboid;
 
 import ch.n1b.worldedit.schematic.schematic.Cuboid;
 
+import java.util.function.Function;
+
 /**
  * Created on 07.01.2015.
  *
  * @author Thomas
  */
-public class CuboidRotator {
+public class CuboidRotator implements Function<Cuboid,Cuboid>{
+    @Override
     public Cuboid apply(Cuboid cuboid){
         cuboid.rotate2D(90);
         return cuboid;
